5d9853026e1716a9ba1497182756c32881f06e77
Wyatt Zeimis <wzeimis15@apu.edu>
2020-01-08 21:51:04 -0800

    Initial check in of TeamSearch Project

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import { StyleSheet, Text, View, ClippingRectangle, Button } from 'react-native';
+import { button } from 'react-native-elements';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>It works bitch!</Text>
+      <Button title='button bitch' icon={{ name: 'code' }} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
