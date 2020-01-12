e127b9b87b8b12802f9c3ee8fc4e137fd6986d0f
Erin Morikawa <emorikawa15@apu.edu>
2020-01-12 15:35:10 -0800

    Added server and database connection, plus test seed data

---
+++ server/seed/data.js
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    // used to create docs in db
+    title: "t1",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    title: "t2",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing..."
+  }
+];
