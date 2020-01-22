0880e16f7e82cda8cf623000a64ffda15eb8bec5
cecilialuna11 <32347620+cecilialuna11@users.noreply.github.com>
2020-01-22 13:45:44 -0800

    created header component.

--- mobile/App/components/header.js
+++ mobile/App/components/header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Stylesheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Header() {
     return (
         <View styles={ styles.header }>
         <View>
-            <Text style={styles.headerText}></Text>
+            <Text style={styles.headerText}>Team Info</Text>
         </View>
         </View>
     )
@@ -17,6 +17,14 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
         flexDirection: 'row',
-        alingItmes: 'center'
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'red',
+    },
+    headerText: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: '#333',
+        letterSpacing: 1,
     }
 })
--- mobile/App/index.js
+++ mobile/App/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer, AppRegistry } from "react-navigation";
+import Header from './components/header';
+import {
+  ScrollView,
+  StatusBar,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView
+} from "react-native";
+import React, { Component } from "react";
 
+import teamInfo from "./screens/teamInfo";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white"
+  }
+});
+
+const MainStack = createStackNavigator({
+  TeamInfo: {
+    screen: teamInfo,
+    navigationOptions: {
+      headerTitle: () => <Header/>,
+    }
   },
 });
 
-export default () => (
-  <View style={styles.container}>
-    <Text>Open up App.js to start working on your app!</Text>
-  </View>
-);
+export default createAppContainer(MainStack);
+
+class App extends Component {
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.text}> Welcome to TeamSearch! </Text>
+      </SafeAreaView>
+    );
+  }
+}
