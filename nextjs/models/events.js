c7a39d7b4b942302538d65a4ed98702aceba0dbd
Wyatt Zeimis <wzeimis15@apu.edu>
2020-02-07 13:31:16 -0800

    graphql tutorial video #5

---
+++ nextjs/models/events.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Event", eventSchema);
--- nextjs/util/configureApi.js
+++ nextjs/util/configureApi.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const helmet = require('helmet');
-const moment = require('moment');
-const bodyParser = require('body-parser');
+const express = require("express");
+const helmet = require("helmet");
+//const moment = require('moment');
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -9,4 +12,84 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-module.exports = app;
+const Event = require("./models/event");
+
+//const events = [];
+
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+          type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+          }
+          input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+          }
+          type RootQuery {
+              events: [Event!]!
+          }
+          type RootMutation {
+              createEvent(eventInput: EventInput): Event
+          }
+          schema {
+              query: RootQuery
+              mutation: RootMutation
+          }
+      `),
+    rootValue: {
+      events: () => {
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
+      },
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      }
+    },
+    graphiql: true
+  })
+);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-ntrwp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+//app.listen(3000);
+
+//module.exports = app;
