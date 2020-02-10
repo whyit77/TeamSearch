2eae3c7ee808beb0df51d0b36b074ed61d2c865d
rclee15 <rclee15@apu.edu>
2020-02-09 21:20:44 -0800

    Map library changes

--- mobile/App/screens/Map.js
+++ mobile/App/screens/Map.js
@@ -2,37 +2,47 @@ import React, { Component } from "react";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { Marker } from "react-native-maps";
 
-import HeatMap from "../components/HeatMap";
+import Heatmap from "react-native-simpleheat";
+import WebView from "react-native";
 
 class Map extends React.Component {
-  state = {
-    loading: false,
-    location: null,
-    showMap: false
-  };
-
-  render() {
-    const { location, loading } = this.state;
-
+  // Write this as JavaScript???
+  HeatMap() {
     return (
-      <HeatMap
+      <Heatmap>
+        WebView={WebView}
+        region=
+        {{
+          latitude: 34.1301,
+          longitude: -117.8884,
+          latitudeDelta: 0.00922,
+          longitudeDelta: 0.00421
+        }}
+        data={[-117.8884, 34.1301, 10]}
+      </Heatmap>
+    );
+  }
+  render() {
+    return (
+      <MapView
         style={{ flex: 1 }}
         provider={PROVIDER_GOOGLE}
         showsUserLocation
         initialRegion={{
           latitude: 34.1301,
-          longitude: 117.8884,
+          longitude: -117.8884,
           latitudeDelta: 0.00922,
           longitudeDelta: 0.00421
         }}
+        onRegionChange={this.HeatMap()}
       >
-        <Marker
+        {/* <Marker
           coordinate={{
             latitude: 34.1301,
-            longitude: 117.8884
+            longitude: -117.8884
           }}
-        />
-      </HeatMap>
+        /> */}
+      </MapView>
     );
   }
 }
