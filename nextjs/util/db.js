57854192be1a22dade2adc04097427c25dbd827a
bellsarian <bellasarian@gmail.com>
2020-02-17 18:52:06 -0800

    GridFS Research

--- nextjs/util/db.js
+++ nextjs/util/db.js
@@ -6,7 +6,8 @@ let isConnected;
 // REMOVE PASSWORD BEFORE PUSHING <password>
 // REPLACE PASSWORD TO RUN
 const DB_URL =
-  "mongodb+srv://Erin1:DrDan!480@teamsearch1-8sc8z.mongodb.net/test?retryWrites=true&w=majority";
+  // "mongodb+srv://Erin1:DrDan!480@teamsearch1-8sc8z.mongodb.net/test?retryWrites=true&w=majority";
+  "mongodb+srv://bsarian:Llebysysysys123@teamsearch1-8sc8z.mongodb.net/test?retryWrites=true&w=majority";
 
 const connectToDatabase = () => {
   if (isConnected) {
