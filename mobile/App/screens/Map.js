07691561be7a1694b267b24442bb418063b84aa3
rclee15 <rclee15@users.noreply.github.com>
2020-02-24 11:29:13 -0800

    Gradient addition

--- mobile/App/screens/Map.js
+++ mobile/App/screens/Map.js
@@ -1,84 +1,98 @@
 import React, { Component } from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { Marker } from "react-native-maps";
-import { Overlay } from "react-native-elements";
+// import { Overlay } from "react-native-elements";
 
 const styles = StyleSheet.create({
-  map: {
-    flex: 1,
-    opacity: 0.5
-  },
   overlay: {
-    flex: 1
-    // opacity: 0.5,
-    // backgroundColor: "blue"
+    flex: 1,
+    backgroundColor: "blue"
   }
 });
 
 class Map extends React.Component {
   render() {
+    const gradientHeight = 500;
+    const gradientBackground = "red";
+    const data = Array.from({ length: gradientHeight });
+
     return (
-      <Overlay
-        isVisible={true}
-        overlayBackgroundColor="red"
-        fullScreen={true}
-        overlayStyle={styles.overlay}
+      // <Overlay
+      //   isVisible={true}
+      //   overlayBackgroundColor="red"
+      //   fullScreen={true}
+      //   overlayStyle={styles.overlay}
+      // >
+      <MapView
+        customMapStyle={[
+          {
+            featureType: "road.local",
+            elementType: "geometry.fill",
+            stylers: [
+              {
+                color: "#ff8080"
+              },
+              {
+                visibility: "on"
+              },
+              {
+                weight: 5
+              }
+            ]
+          },
+          {
+            featureType: "landscape.natural.landcover",
+            elementType: "geometry.fill",
+            stylers: [
+              {
+                color: "#3d84ff"
+              },
+              {
+                saturation: -50
+              },
+              {
+                lightness: 25
+              },
+              {
+                visibility: "on"
+              }
+            ]
+          }
+        ]}
+        style={styles.overlay}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        initialRegion={{
+          latitude: 34.1301,
+          longitude: -117.8884,
+          latitudeDelta: 0.00922,
+          longitudeDelta: 0.00421
+        }}
       >
-        <MapView
-          customMapStyle={[
-            {
-              featureType: "road.local",
-              elementType: "geometry.fill",
-              stylers: [
-                {
-                  color: "#ff8080"
-                },
-                {
-                  visibility: "on"
-                },
-                {
-                  weight: 5
-                }
-              ]
-            },
-            {
-              featureType: "landscape.natural.landcover",
-              elementType: "geometry.fill",
-              stylers: [
-                {
-                  color: "#3d84ff"
-                },
-                {
-                  saturation: -50
-                },
-                {
-                  lightness: 25
-                },
-                {
-                  visibility: "on"
-                }
-              ]
-            }
-          ]}
-          style={styles.map}
-          provider={PROVIDER_GOOGLE}
-          showsUserLocation
-          initialRegion={{
-            latitude: 34.1301,
-            longitude: -117.8884,
-            latitudeDelta: 0.00922,
-            longitudeDelta: 0.00421
-          }}
-        >
-          <Marker
-            coordinate={{
-              latitude: 34.1301,
-              longitude: -117.8884
+        {data.map((_, i) => (
+          <View
+            key={i}
+            style={{
+              position: "relative",
+              backgroundColor: gradientBackground,
+              height: 0.25,
+              // bottom: gradientHeight - i,
+              right: 0,
+              left: 0,
+              zIndex: 2,
+              opacity: (1 / gradientHeight) * (i + 1)
             }}
           />
-        </MapView>
-      </Overlay>
+        ))}
+        <Marker
+          coordinate={{
+            latitude: 34.1301,
+            longitude: -117.8884
+          }}
+        />
+      </MapView>
+      // </Overlay>
     );
   }
 }
