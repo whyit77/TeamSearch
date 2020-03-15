68dbb6ea162ddb68a394e9557cb176b141a83d63
rclee15 <rclee15@users.noreply.github.com>
2020-03-14 21:46:35 -0700

    Implemented react native maps heatmap

--- mobile/App/screens/Map.js
+++ mobile/App/screens/Map.js
@@ -1,169 +1,87 @@
-// import React, { Component } from "react";
-// import { StyleSheet, View } from "react-native";
-// import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
-// import { Marker } from "react-native-maps";
-
-// import { EmbeddedWebView } from "../components/EmbeddedWebView";
-
-// class Map extends React.Component {
-//   render() {
-//     const gradientHeight = 500;
-//     const gradientBackground = "red";
-//     const data = Array.from({ length: gradientHeight });
-
-//     return <EmbeddedWebView url={"http://localhost:8000/"} />;
-//   }
-// }
-
-// export default Map;
-
-import React, { Component } from "react";
-import { StyleSheet, View, Dimensions, Text } from "react-native";
+import React from "react";
+import { StyleSheet } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
-import { Marker } from "react-native-maps";
-// import { Overlay } from "react-native-elements";
+import { Heatmap } from "react-native-maps";
 
 const styles = StyleSheet.create({
-  overlay: {
-    flex: 1,
-    backgroundColor: "blue"
-  },
-  marker: {
-    backgroundColor: "#550bbc",
-    padding: 5,
-    borderRadius: 5
-  },
-  text: {
-    color: "#FFF",
-    fontWeight: "bold"
+  map: {
+    flex: 1
   }
 });
 
-function getRandomKey(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
 class Map extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    initialPosition: {
+      latitude: 40.7143,
+      longitude: -74.0042,
+      latitudeDelta: 0.09,
+      longitudeDelta: 0.035
+    }
+  };
 
-    this.state = {
-      markers: []
-    };
-
-    this.handlePress = this.handlePress.bind(this);
-  }
-
-  handlePress(e) {
-    this.setState({
-      markers: [
-        ...this.state.markers,
-        {
-          coordinate: e.nativeEvent.coordinate,
-          key: getRandomKey(0, 180)
-        }
-      ]
-    });
-  }
+  points = [
+    { latitude: 40.7828, longitude: -74.0065, weight: 1 },
+    { latitude: 41.7121, longitude: -74.0042, weight: 1 },
+    { latitude: 40.7102, longitude: -75.006, weight: 1 },
+    { latitude: 40.7123, longitude: -74.0052, weight: 1 },
+    { latitude: 40.7032, longitude: -74.0042, weight: 1 },
+    { latitude: 40.7198, longitude: -74.0024, weight: 1 },
+    { latitude: 41.7223, longitude: -74.0053, weight: 1 },
+    { latitude: 40.7181, longitude: -74.0042, weight: 1 },
+    { latitude: 40.7124, longitude: -74.0023, weight: 1 },
+    { latitude: 40.7648, longitude: -74.0012, weight: 1 },
+    { latitude: 41.7128, longitude: -74.0027, weight: 1 },
+    { latitude: 40.7223, longitude: -74.0153, weight: 1 },
+    { latitude: 40.7193, longitude: -74.0052, weight: 1 },
+    { latitude: 40.7241, longitude: -75.0013, weight: 1 },
+    { latitude: 41.7518, longitude: -74.0085, weight: 1 },
+    { latitude: 40.7599, longitude: -74.0093, weight: 1 },
+    { latitude: 41.7523, longitude: -74.0021, weight: 1 },
+    { latitude: 40.7342, longitude: -74.0152, weight: 1 },
+    { latitude: 40.7484, longitude: -75.0042, weight: 1 },
+    { latitude: 40.7929, longitude: -75.0023, weight: 1 },
+    { latitude: 40.7292, longitude: -74.0013, weight: 1 },
+    { latitude: 40.794, longitude: -74.0048, weight: 1 },
+    { latitude: 40.7874, longitude: -74.0052, weight: 1 },
+    { latitude: 40.7824, longitude: -74.0024, weight: 1 },
+    { latitude: 40.7232, longitude: -74.0094, weight: 1 },
+    { latitude: 41.7342, longitude: -74.0152, weight: 1 },
+    { latitude: 41.7484, longitude: -74.0012, weight: 1 },
+    { latitude: 41.7929, longitude: -74.0073, weight: 1 },
+    { latitude: 41.7292, longitude: -74.0013, weight: 1 },
+    { latitude: 41.794, longitude: -74.0058, weight: 1 },
+    { latitude: 41.7874, longitude: -74.0352, weight: 1 },
+    { latitude: 41.7824, longitude: -74.0024, weight: 1 },
+    { latitude: 41.7232, longitude: -74.0094, weight: 1 },
+    { latitude: 41.0342, longitude: -75.0152, weight: 1 },
+    { latitude: 41.0484, longitude: -75.0012, weight: 1 },
+    { latitude: 41.0929, longitude: -75.0073, weight: 1 },
+    { latitude: 41.0292, longitude: -74.0013, weight: 1 },
+    { latitude: 41.094, longitude: -74.0068, weight: 1 },
+    { latitude: 41.0874, longitude: -74.0052, weight: 1 },
+    { latitude: 41.0824, longitude: -74.0024, weight: 1 },
+    { latitude: 41.0232, longitude: -74.0014, weight: 1 }
+  ];
 
   render() {
-    const gradientHeight = 500;
-    const gradientBackground = "red";
-    const data = Array.from({ length: gradientHeight });
-    let { width, height } = Dimensions.get("window");
-
     return (
-      // <Overlay
-      //   isVisible={true}
-      //   overlayBackgroundColor="red"
-      //   fullScreen={true}
-      //   overlayStyle={styles.overlay}
-      // >
-
       <MapView
-        customMapStyle={[
-          {
-            featureType: "landscape.natural.landcover",
-            elementType: "geometry.fill",
-            stylers: [
-              {
-                color: "#3d84ff"
-              },
-              {
-                saturation: -50
-              },
-              {
-                lightness: 25
-              },
-              {
-                visibility: "on"
-              }
-            ]
-          }
-        ]}
-        style={styles.overlay}
         provider={PROVIDER_GOOGLE}
-        showsUserLocation
-        initialRegion={{
-          latitude: 34.1301,
-          longitude: -117.8884,
-          latitudeDelta: 0.00922,
-          longitudeDelta: 0.00421
-        }}
-        onPress={this.handlePress}
+        ref={map => (this._map = map)}
+        style={styles.map}
+        initialRegion={this.state.initialPosition}
       >
-        <View
-          style={{
-            position: "absolute",
-            justifyContent: "center",
-            // Notes: Dimensions must be pulled in from user data
-            // top: -width / 2 + 100,
-            // left: -width / 2 + 50,
-            // right: -width / 2 + 50,
-            // bottom: -width / 2 + 200,
-            backgroundColor: "transparent",
-
-            borderWidth: width / 4,
-            borderRadius: width,
-            borderColor: "red",
-            opacity: 0.3
+        <Heatmap
+          initialRegion={this.state.initialPosition}
+          points={this.points}
+          radius={40}
+          gradient={{
+            colors: ["black", "purple", "red", "yellow", "white"],
+            startPoints: [0.01, 0.04, 0.1, 0.45, 0.5],
+            colorMapSize: 200
           }}
-        >
-          {/* {data.map((_, i) => (
-            <View
-              key={i}
-              style={{
-                position: "relative",
-                backgroundColor: gradientBackground,
-                height: 0.25,
-                // bottom: gradientHeight - i,
-                right: 0,
-                left: 0,
-                zIndex: 2,
-                opacity: (1 / gradientHeight) * (i + 1)
-              }}
-            />
-          ))} */}
-
-          {this.state.markers.map(marker => {
-            return (
-              <View style={styles.marker}>
-                <Text style={styles.text}>Pin</Text>
-              </View>
-            );
-          })}
-
-          {/* <Marker
-            coordinate={{
-              latitude: 34.1301,
-              longitude: -117.8884
-            }}
-          /> */}
-        </View>
+        ></Heatmap>
       </MapView>
-      // </Overlay>
     );
   }
 }
