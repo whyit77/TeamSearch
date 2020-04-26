39e9c69de4a58050885d566eaa4b78d5285cb7f5
bellsarian <bellasarian@gmail.com>
2020-04-25 21:49:37 -0700

    REBECCA IS GREAT SHE MADE THE NAV WORK WELL

--- mobile/App/routes/loginStack.js
+++ mobile/App/routes/loginStack.js
@@ -1,8 +1,14 @@
 import { createStackNavigator } from "react-navigation-stack";
 import Login from "../screens/Login";
+import CreateAccount from "../screens/CreateAccount";
+import TeamListView from "../screens/TeamList";
+import CreateTeam from "../screens/CreateTeam";
+import UserSettings from "../screens/UserSettings";
+
 import Header from "../components/Header";
 import React from "react";
 import TeamList from "../screens/TeamList";
+import HiddenDrawerItem from "../components/HiddenDrawerItem";
 // import ResetPW from "../screens/ResetPassword";
 
 const screens = {
@@ -11,19 +17,65 @@ const screens = {
     navigationOptions: () => {
       return {
         headerTitle: "Login",
+        headerTintColor: "white",
+        drawerLabel: <HiddenDrawerItem />
+      };
+    }
+  },
+  CreateAccount: {
+    screen: CreateAccount,
+    navigationOptions: ({ navigation }) => {
+      // return {
+      //   headerTitle: () => (
+      //     <Header navigation={navigation} title="Create Account" />
+      //   )
+      return {
+        headerTitle: "Create Account",
         headerTintColor: "white"
       };
     }
   },
-  TeamList: {
-    screen: TeamList,
-    navigationOptions: () => {
+  TeamListView: {
+    screen: TeamListView,
+    navigationOptions: ({ navigation }) => {
       return {
-        headerTitle: "Team List",
-        headerTintColor: "white"
+        headerTitle: () => <Header navigation={navigation} title="Team List" />
+      };
+    }
+  },
+  CreateTeam: {
+    screen: CreateTeam,
+    navigationOptions: ({ navigation }) => {
+      return {
+        headerTitle: () => (
+          <Header navigation={navigation} title="Create Team" />
+        )
+      };
+    }
+    // navigationOptions: () => {
+    //   return {
+    //     headerTitle: "Create Team",
+    //     headerTintColor: "white"
+    //   };
+    // }
+  },
+  UserSettings: {
+    screen: UserSettings,
+    navigationOptions: ({ navigation }) => {
+      return {
+        headerTitle: () => <Header navigation={navigation} title="User Profile" />
       };
     }
   }
+  // TeamList: {
+  //   screen: TeamList,
+  //   navigationOptions: () => {
+  //     return {
+  //       headerTitle: "Team List",
+  //       headerTintColor: "white"
+  //     };
+  //   }
+  // }
   // ResetPW: {
   //   screen: ResetPW,
   //   navigationOptions: () => {
